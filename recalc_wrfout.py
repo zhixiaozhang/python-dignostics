644e404eee7385aa5ce9a51c831f18a713ed1d54
zhixiaozhang <72099269+zhixiaozhang@users.noreply.github.com>
2021-09-21 12:30:05 -0600

    Update recalc_wrfout.py

--- recalc_wrfout.py
+++ recalc_wrfout.py
@@ -2,7 +2,7 @@
 # Copy module_diag_functions.f90 and recalc_wrfout.py in the same working directory
 # Run Linux command: python -m numpy.f2py -c module_diag_functions.f90 -m diag_functions to add diagnostic functions to numpy.f2py
 # Notes: afwa.diag_functions.diag_map and afwa.diag_functions.diag_row return the 2-D diagnostic map and row, respectively.
-# When the model domain is large, diag_row is recommended to be used.
+# When the model domain is large, diag_row is recommended to be used with proper parallelization.
 # zhixiao.zhang@utah.edu, 2021/09/21
 
 from multiprocessing import Pool
